819e40f3e0a5ead08b14fb4814065b661a1e3288
ilyas-g <ilyas15069i@gmail.com>
2023-10-12 17:58:56 +0200

    Installation de Tailwind CSS

--- src/App.vue
+++ src/App.vue
@@ -15,36 +15,14 @@ import Header from './components/Header.vue'
   
   <main>
     <Header headerTest="okokok" />
+    <h1 class="text-3xl font-bold underline bg-red-200">
+    Hello world!
+  </h1>
     <!-- <TheWelcome /> -->
   </main>
   <router-view></router-view>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
