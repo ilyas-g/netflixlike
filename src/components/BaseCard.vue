1560442fa142138e43b9aa4b210c906cdc743d24
ilyas-g <ilyas15069i@gmail.com>
2023-10-15 01:13:07 +0200

    Ajout d'un composant BaseCard et fetch des datas pour les films du moments

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Base Card {{ ok }}
+    </div>
+</template>
+<script setup lang="ts">
+defineProps<{
+  ok: string
+}>()
+</script>
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,45 @@
-<template>
-    <div>
-        <h1 class="text-4xl">Homeview {{ homeTest }}</h1>
-    </div>
-</template>
 <script setup lang="ts">
-defineProps<{
-  homeTest: string
-}>()
+
+import { ref } from 'vue';
+import BaseCard from '@/components/BaseCard.vue'
+
+const token = import.meta.env.VITE_GRAY_TOKEN;
+
+const product: any = ref(null);
+const response: any = ref(null);
+
+function getImage(imagePath: string) {
+  const baseURL = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2';
+  return baseURL + imagePath;
+}
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${token}`
+  }
+};
+
+fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+  .then(response => response.json())
+  .then(response => product.value = response)
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+
 </script>
-<style lang="">
-    
-</style>
+
+<template>
+  <div>
+      <h1 class="text-4xl">Homeview</h1>
+
+      <BaseCard ok="lp" />
+      <p>Product name: {{product?.dates.maximum}}</p>
+      <ul class="mb-0">
+          <li v-for="produc in product.results" :key="produc.id">
+            Popularity: {{produc.original_title}}<br/>
+            <img class="w-40" :src="getImage(produc.poster_path)" :alt="produc.original_title" />
+          </li>
+      </ul>
+  </div>
+</template>
