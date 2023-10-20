16d02954e207f06cdc829d71e059d5af2df66c0a
ilyas-g <ilyas15069i@gmail.com>
2023-10-20 19:45:07 +0200

    Système de tabulation dans la page de listing fonctionnel

--- src/components/BaseTabs.vue
+++ src/components/BaseTabs.vue
@@ -1,21 +1,24 @@
 <template>
-  <li v-on:click="activetab=1" role="presentation" class="mr-2" v-bind:class="[ activetab === tabNumber ? 'active' : '' ]">
-    <button class="inline-block p-4 hover:border-b-2 rounded-t-lg hover:border-gray-300 dark:hover:text-gray-300" 
+  <li role="presentation" class="mr-2" v-bind:class="[ activetab === tabNumber ? 'active' : '' ]">
+    <button @click="$emit('someEvent')" class="inline-block p-4 hover:border-b-2 rounded-t-lg hover:border-gray-300 dark:hover:text-gray-300" 
     v-bind:class="[ activetab === tabNumber ? 'border-b-2 text-gray-300' : '' ]" id="profile-tab" data-tabs-target="#profile" 
     type="button" role="tab" aria-controls="profile" aria-selected="false">{{ title }}</button>
 </li>
 </template>
-<script setup lang="ts">
+
+<script lang="ts">
 import { ref } from 'vue';
-defineProps<{
-  title: string
-  tabNumber: number
-}>()
-const activetab = ref(1);
 
+export default {
+  props: {
+    title: String,
+    tabNumber: Number
+  },
+  setup() {
+    const activetab = ref(1);
+    return {
+      activetab
+    }
+  }
+}
 </script>
-
-<style>
-/* Style the tab content */
-
-</style>
--- src/views/ListingView.vue
+++ src/views/ListingView.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import BaseCard from '@/components/BaseCard.vue'
 import BaseTabs from '@/components/BaseTabs.vue'
-import { ref } from 'vue';
-const movies: any = ref({});
+import { ref, watchEffect } from 'vue';
+
 const options = {
   method: 'GET',
   headers: {
@@ -10,25 +10,30 @@ const options = {
     Authorization: `Bearer ${import.meta.env.VITE_GRAY_TOKEN}`
   }
 };
-async function getMoviesData() {
-  const res = await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=99', options);
-  const finalRes = await res.json();
-  console.log(finalRes);
-  movies.value = finalRes;
-}
+
 function getImage(imagePath: string) {
   const baseURL = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2';
   return baseURL + imagePath;
 }
-getMoviesData()
 
-const posts = ref([
-      { id: 28, titlee: 'Action' },
-      { id: 16, titlee: 'Animation' },
-      { id: 12, titlee: 'Aventure' },
-      { id: 35, titlee: 'Comédie' },
-      { id: 99, titlee: 'Documentaire' }
-    ])
+const branches:any = ref([
+  { id: 28, title: 'Action' },
+  { id: 16, title: 'Animation' },
+  { id: 12, title: 'Aventure' },
+  { id: 35, title: 'Comédie' },
+  { id: 99, title: 'Documentaire' }
+])
+
+const currentBranch = ref(branches[0])
+const datas:any = ref({})
+
+const API_URL = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=`
+watchEffect(async () => {
+  const response = await fetch(
+    `${API_URL}${currentBranch.value}`, options
+  )
+  datas.value = await response.json()
+})
 </script>
 
 <template>
@@ -36,15 +41,14 @@ const posts = ref([
         <h1 class="text-5xl">ListingView</h1>
         <div class="mb-4 border-b border-gray-200 dark:border-gray-700">
           <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="myTab" data-tabs-toggle="#myTabContent" role="tablist">
-            <!-- <p>{{ post.titlee }}</p> -->
-            <BaseTabs v-for="post in posts" :title="post.titlee" :tabNumber="post.id">{{ post.titlee }}</BaseTabs>
+            <BaseTabs v-for="branch in branches" :title="branch.title" :tabNumber="branch.id" @some-event="() => {currentBranch = branch.id}" />
           </ul>
         </div>
-        <!-- <BaseTabs title="mpm" :tabNumber=1>okolm</BaseTabs> -->
         <div class="mx-auto grid grid-cols-4 gap-5 place-items-end h-56 mb-4 p-5">
-      <template v-for="(movie, index) in movies.results" :key="movie.id">
+      <template v-for="(movie, index) in datas.results" :key="movie.id">
 
           <BaseCard
+          :index="movie.id"
           :title="movie.original_title" 
           :imgSrc="getImage(movie.poster_path)"
           :overview="movie.overview" />
