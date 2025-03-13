e17fb0bfda3f40e1c73c06210ecc89232b5cf504
ilyas-g <ilyas15069i@gmail.com>
2025-03-13 13:31:04 +0100

    Update BrowsersList

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -9,4 +9,4 @@ export const options = {
 export function getImage(imagePath: string) {
     const baseURL = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2';
     return baseURL + imagePath;
-  }
+}
